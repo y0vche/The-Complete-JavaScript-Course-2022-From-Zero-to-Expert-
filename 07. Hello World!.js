53563e92f49f79e03606a7f97691f0b491445f8e
yovche <70894119+y0vche@users.noreply.github.com>
2023-04-17 11:53:09 +0200

    Add files via upload

---
+++ 07. Hello World!.js
@@ -0,0 +1,21 @@
+ 
+// LESSON 7. Hello World! (05m57s)
+
+// --------------------
+//  alert('Hello ss!')
+
+// --------------------
+ let js = 'amazing'
+
+ if (js ==='amazing') alert('JavaScript is FUN!')
+
+// --------------------
+js = 'boring'
+
+// if (js ==='amazing') alert('JavaScript is FUN!')
+
+// Click UP ARROW key to go to the previous command in the browser Cosole window 
+ 
+
+// --------------------
+// console.log(40 + 8 + 23 - 10);
