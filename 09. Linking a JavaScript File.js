06fab4b5ef3f8ee85d1863083b191d44c0acf3b9
yovche <70894119+y0vche@users.noreply.github.com>
2023-04-18 20:40:06 +0200

    Add files via upload

---
+++ 09. Linking a JavaScript File.js
@@ -0,0 +1,8 @@
+// LESSON 9. Linking a JavaScript File (15m55s)
+
+//--------------------
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+
+//--------------------
+console.log(40 + 8 + 23 - 10);
