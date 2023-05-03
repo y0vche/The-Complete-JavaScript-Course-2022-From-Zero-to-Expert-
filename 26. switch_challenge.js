bcf9157111ed07a25d818d625c59b232ce1a134a
yovche <70894119+y0vche@users.noreply.github.com>
2023-05-03 09:48:45 +0200

    first commit

---
+++ 26. switch_challenge.js
@@ -0,0 +1,18 @@
+// Write the same logic of Switch assigment using if-else statement
+
+const day = "saturday";
+
+if (day ==='monday') {
+  console.log("Plan course structure");
+  console.log("go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day ==="wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day ==="saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else  
+  console.log("Not a valid day");
+ 
